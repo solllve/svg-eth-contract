f00c3981eccf9f88b089df7d8df8ca21e0a05647
solllve <caseyhald@Caseys-MacBook-Pro.local>
2022-02-15 10:31:47 -0800

    init

---
+++ scripts/run.js
@@ -0,0 +1,32 @@
+
+const main = async () => {
+    // This will actually compile our contract and generate the necessary files we need to work with our contract under the artifacts directory. Go check it out after you run this :).
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+    //What's happening here is Hardhat will create a local Ethereum network for us, but just for this contract. Then, after the script completes it'll destroy that local network. So, every time you run the contract, it'll be a fresh blockchain. Whats the point? It's kinda like refreshing your local server every time so you always start from a clean slate which makes it easy to debug errors.
+    const nftContract = await nftContractFactory.deploy();
+    //We'll wait until our contract is officially mined and deployed to our local blockchain! That's right, hardhat actually creates fake "miners" on your machine to try its best to imitate the actual blockchain.
+    //Our constructor runs when we actually are fully deployed!
+    await nftContract.deployed();
+    console.log('Contract Deployed to:', nftContract.address);
+    // Call the function.
+    let txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+
+    // Mint another NFT for fun.
+    txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+};
+
+const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
